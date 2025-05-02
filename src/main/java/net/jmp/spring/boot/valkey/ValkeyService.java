2215016f33446a920a4787e28f088d275882dfea
Jonathan Parker <jonathanp62@gmail.com>
2025-05-02 09:09:30 -0400

    Reworked the examples and started to add more API demonstrations

--- src/main/java/net/jmp/spring/boot/valkey/ValkeyService.java
+++ src/main/java/net/jmp/spring/boot/valkey/ValkeyService.java
@@ -32,13 +32,15 @@ import glide.api.GlideClient;
 
 import static glide.api.models.GlideString.gs;
 
+import glide.api.models.GlideString;
 import glide.api.models.configuration.GlideClientConfiguration;
 import glide.api.models.configuration.NodeAddress;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-import static net.jmp.util.logging.LoggerUtils.entry;
-import static net.jmp.util.logging.LoggerUtils.exit;
+import static net.jmp.util.logging.LoggerUtils.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,6 +53,25 @@ import org.springframework.stereotype.Service;
 ///
 /// @version    0.1.0
 /// @since      0.1.0
+///
+/// APIs to get acquainted with:
+///  client.copy();
+///  client.customCommand();
+///  client.exec();
+///  client.fcall();
+///  client.function...();
+///  client.lastsave();
+///  client.lolwut();
+///
+/// Data types to get acquainted with:
+///  String
+///  Hash
+///  List
+///  Set
+///  Sorted Set
+///  Vector Set
+///  JSON
+///  Serialized Objects (Kryo, JSON, Java)
 @Service
 public class ValkeyService {
     /// The logger.
@@ -79,19 +100,6 @@ public class ValkeyService {
             this.logger.trace(entry());
         }
 
-        this.glideExamples();
-
-        if (this.logger.isTraceEnabled()) {
-            this.logger.trace(exit());
-        }
-    }
-
-    /// The glide examples method.
-    private void glideExamples() {
-        if (this.logger.isTraceEnabled()) {
-            this.logger.trace(entry());
-        }
-
         final GlideClientConfiguration config =
                 GlideClientConfiguration.builder()
                         .address(NodeAddress.builder()
@@ -103,36 +111,112 @@ public class ValkeyService {
                         .build();
 
         try (final GlideClient client = GlideClient.createClient(config).get()) {
-            this.logger.info("PING: {}", client.ping(gs("PING")).get());
-            this.logger.info("PING(found you): {}", client.ping(gs("found you")).get());
+            this.logger.info("CLIENTGETNAME: {}", client.clientGetName().get());    // Returns null
+            this.logger.info("CLIENTID: {}", client.clientId().get());              // Returns 12
+            this.logger.info("INFO: {}", client.info().get());
 
-            this.logger.info("SET(apples, oranges): {}", client.set(gs("apples"), gs("oranges")).get());
-            this.logger.info("GET(apples): {}", client.get(gs("apples")).get());
+            this.echoAndPing(client);
+            this.getAndSet(client);
+            this.getAndDelete(client);
+            this.hash(client);
 
-            /*
-             * APIs to get acquainted with:
-             *
-             *  client.clientGetName();
-             *  client.clientId();
-             *  client.copy();
-             *  client.customCommand();
-             *  client.dbsize();
-             *  client.echo();
-             *  client.exec();
-             *  client.fcall();
-             *  client.flushall();
-             *  client.flushdb();
-             *  client.function...();
-             *  client.info();
-             *  client.lastsave();
-             *  client.lolwut();
-             */
+            client.flushall();
+
+            this.logger.info("DBSIZE: {}", client.dbsize().get());  // Returns 0
         } catch (final ExecutionException | InterruptedException e) {
             if (e instanceof InterruptedException) {
                 Thread.currentThread().interrupt();
             }
 
             this.logger.error("Glide example failed with an exception: {}", e.getMessage(), e);
+        }
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Echo and ping commands.
+    ///
+    /// @param  client  glide.api.GlideClient
+    private void echoAndPing(final GlideClient client) throws ExecutionException, InterruptedException {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(client));
+        }
+
+        this.logger.info("PING: {}", client.ping(gs("PING")).get());
+        this.logger.info("ECHO(back at you): {}", client.echo(gs("back at you")).get());
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Get and set commands.
+    ///
+    /// @param  client  glide.api.GlideClient
+    private void getAndSet(final GlideClient client) throws ExecutionException, InterruptedException {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(client));
+        }
+
+        this.logger.info("SET(apples, oranges): {}", client.set(gs("apples"), gs("oranges")).get());    // Returns OK
+        this.logger.info("GET(apples): {}", client.get(gs("apples")).get());
+        this.logger.info("GET(oranges): {}", client.get(gs("oranges")).get());  // Returns null
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Get and delete commands.
+    ///
+    /// @param  client  glide.api.GlideClient
+    private void getAndDelete(final GlideClient client) throws ExecutionException, InterruptedException {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(client));
+        }
+
+        final GlideString name = gs("name");
+
+        this.logger.info("SET(name, Jonathan): {}", client.set(name, gs("Jonathan")).get());    // Returns OK
+        this.logger.info("GET(name): {}", client.get(name).get());
+        this.logger.info("GETDEL(name): {}", client.getdel(name).get());    // Returns Jonathan
+        this.logger.info("GET(name): {}", client.get(name).get());          // Returns null
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exit());
+        }
+    }
+
+    /// Hash commands.
+    ///
+    /// @param  client  glide.api.GlideClient
+    private void hash(final GlideClient client) throws ExecutionException, InterruptedException {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entryWith(client));
+        }
+
+        final Map<GlideString, GlideString> map = Map.of(
+                gs("firstName"), gs("Jonathan"),
+                gs("lastName"), gs("Parker")
+        );
+
+        final GlideString myHash = gs("my-hash");
+
+        this.logger.info("HSET(my-hash, map): {}", client.hset(myHash, map).get());    // Returns 2 if not already created, else 0
+
+        final GlideString[] keys = client.hkeys(myHash).get();
+        final GlideString lastName = gs("lastName");
+
+        if (this.logger.isInfoEnabled()) {
+            this.logger.info("HKEYS(my-hash): {}", Arrays.toString(keys));      // Returns [firstName, lastName]
+            this.logger.info("HLEN(my-hash): {}", client.hlen(myHash).get());   // Returns 2
+            this.logger.info("HGET(my-hash, firstName): {}", client.hget(myHash, gs("firstName")).get());
+            this.logger.info("HGET(my-hash, lastName): {}", client.hget(myHash, lastName).get());
+            this.logger.info("HEXISTS(my-hash, lastName): {}", client.hexists(myHash, lastName).get());   // Returns true
+            this.logger.info("HDEL(my-hash, lastName): {}", client.hdel(myHash, new GlideString[] { lastName }).get());  // Returns 1
+            this.logger.info("HGET(my-hash, lastName): {}", client.hget(myHash, lastName).get());   // Returns null
         }
 
         if (this.logger.isTraceEnabled()) {
